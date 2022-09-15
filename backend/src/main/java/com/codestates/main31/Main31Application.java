d2f8bc3d8117b45bfa9bc9cad44c06b615e1684c
minoolian <mw.lee@kakao.com>
2022-09-15 15:43:41 +0900

    Project Init

---
+++ backend/src/main/java/com/codestates/main31/Main31Application.java
@@ -0,0 +1,13 @@
+package com.codestates.main31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main31Application.class, args);
+    }
+
+}
