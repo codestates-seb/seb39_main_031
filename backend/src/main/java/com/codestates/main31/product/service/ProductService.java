304e60cec05a98ac9b2f7e477ac6efd56df74a14
minoolian <mw.lee@kakao.com>
2022-09-20 03:21:14 +0900

    feat: Product Service 로직 추가

---
+++ backend/src/main/java/com/codestates/main31/product/service/ProductService.java
@@ -0,0 +1,68 @@
+package com.codestates.main31.product.service;
+
+import com.codestates.main31.product.entity.Product;
+import com.codestates.main31.product.exception.BusinessLogicException;
+import com.codestates.main31.product.exception.ExceptionCode;
+import com.codestates.main31.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Product readProduct(Long productId) {
+        return findProduct(productId);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Product> readProductsList(int page) {
+        return productRepository.findAll(PageRequest.of(page, 8));
+    }
+
+    // Todo: 작성자를 제외한 인원은 수정 불가능
+    public Product updateProduct(Long productId, Product newProduct) {
+        Product curProduct = findProduct(productId);
+
+        Optional.of(newProduct.getTitle())
+                .ifPresent(title -> curProduct.setTitle(title));
+        Optional.of(newProduct.getBody())
+                .ifPresent(body -> curProduct.setBody(body));
+        Optional.of(newProduct.getEndedTime())
+                .ifPresent(endedTime -> curProduct.setEndedTime(endedTime));
+        Optional.of(newProduct.getGoalNum())
+                .ifPresent(goalNum -> curProduct.setGoalNum(goalNum));
+        Optional.of(newProduct.getGoalPrice())
+                .ifPresent(goalPrice -> curProduct.setGoalPrice(goalPrice));
+        Optional.of(newProduct.getState())
+                .ifPresent(state -> curProduct.setState(state));
+        Optional.of(newProduct.getProductImg())
+                .ifPresent(productImg -> curProduct.setProductImg(productImg));
+
+        return curProduct;
+    }
+
+    // Todo: 작성자를 제외한 인원은 삭제 불가능
+    public void deleteProduct(Long productId) {
+        findProduct(productId);
+        productRepository.deleteById(productId);
+    }
+
+    public Product findProduct(Long productId) {
+        return productRepository.findById(productId).orElseThrow(()-> new BusinessLogicException(ExceptionCode.PRODUCT_NOT_FOUND));
+    }
+
+}
