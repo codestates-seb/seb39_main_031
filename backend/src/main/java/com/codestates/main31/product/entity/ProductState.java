8c0f559b005a593607727a617d019e956604ccba
minoolian <mw.lee@kakao.com>
2022-09-22 01:53:19 +0900

    fix: ProductState Enum 값 수정
    * Scrum 회의에서 진행한 의견 반영

--- backend/src/main/java/com/codestates/main31/product/entity/ProductState.java
+++ backend/src/main/java/com/codestates/main31/product/entity/ProductState.java
@@ -3,13 +3,11 @@ package com.codestates.main31.product.entity;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+//@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ProductState {
-
-    PREPARING("준비중"),
-    PROCEEDING("진행중"),
-    IMMINENT("마감임박"),
-    DEADLINE("마감");
+    PROCEED("진행중"),
+    DEADLINE("모집마감"),
+    TERMINATE("종료");
 
     private final String status;
-
 }
