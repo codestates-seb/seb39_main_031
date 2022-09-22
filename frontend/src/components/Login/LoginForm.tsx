5b9492b64c91f7ba2444a87a80cd528b26bf6c58
shindongjae <sdhsdj6450@gmail.com>
2022-09-22 11:37:36 +0900

    feat: login page input handler

--- frontend/src/components/Login/LoginForm.tsx
+++ frontend/src/components/Login/LoginForm.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import {
+  loginEmailCheck,
+  loginPasswordCheck,
+} from "../../assets/FormCheck/LoginCheckFuc";
 import Button from "../../common/Button/ButtonForm";
 import InputForm from "../../common/Input/InputForm";
 
@@ -13,7 +19,8 @@ const Validation = styled.span`
   margin-top: 5px;
   color: red;
   padding-left: 10px;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
+  font-size: ${({ theme }) => theme.fontSize.size12};
 `;
 
 const LinkContent = styled.div`
@@ -37,16 +44,52 @@ const ButtoneContent = styled.div`
 `;
 
 const LoginForm = () => {
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [validEmail, setValidEmail] = useState<string>("");
+  const [validPassword, setValidPssword] = useState<string>("");
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(userEmail);
+    console.log(password);
   };
+
+  const onChangeEmail = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const email = event.target.value;
+      setUserEmail(email);
+      loginEmailCheck(email, setValidEmail);
+    },
+    [userEmail]
+  );
+
+  const onChangePassword = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const password = event.target.value;
+      setPassword(password);
+      loginPasswordCheck(password, setValidPssword);
+    },
+    [password]
+  );
 
   return (
     <Form onSubmit={handleSubmit}>
-      <InputForm id="userEmail" type="email" lableText="이메일" />
-      <Validation></Validation>
-      <InputForm id="userNickname" type="password" lableText="비밀번호" />
-      <Validation></Validation>
+      <InputForm
+        id="userEmail"
+        type="email"
+        lableText="이메일"
+        onChange={onChangeEmail}
+      />
+      <Validation>{validEmail}</Validation>
+      <InputForm
+        id="userNickname"
+        type="password"
+        lableText="비밀번호"
+        onChange={onChangePassword}
+      />
+      <Validation>{validPassword}</Validation>
       <LinkContent>
         <Link to="/password">
           <span>비밀번호 찾기</span>
