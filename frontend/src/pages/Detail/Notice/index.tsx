d7184d1678c353651b4871a4dd13de521a481029
hxezin <leehxejin@gmail.com>
2022-09-27 01:24:21 +0900

    rename: Notice, ProductInfo 위치 변경

---
+++ frontend/src/pages/Detail/Notice/index.tsx
@@ -0,0 +1,13 @@
+import NoticeButton from "../../../components/Notice/NoticeButton";
+import NoticeEditor from "../../../components/Notice/NoticeEditor";
+
+const Notice = () => {
+  return (
+    <>
+      <NoticeButton />
+      <NoticeEditor />
+    </>
+  );
+};
+
+export default Notice;
